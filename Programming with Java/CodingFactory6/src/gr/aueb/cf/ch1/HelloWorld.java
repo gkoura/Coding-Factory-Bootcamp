8d09c4348ba40867ffe84e517367e03a7078a1ba
Greg <gkouras@icloud.com>
2024-03-26 21:01:38 +0200

    changed gitignore file

---
+++ Programming with Java/CodingFactory6/src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch1; //fqn - fully qualified name
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); //whatever starts with a capital letter is a class in java (e.g. System)
+    }
+}
