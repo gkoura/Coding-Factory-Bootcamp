29e1bd265e12df456a5af0bebdcee62409af9d8b
Greg <gkouras@icloud.com>
2024-09-27 18:50:22 +0300

    finished endpoints for users and products of users

--- 5. NodeJS/2. Class Projects/NodeJS/myApp4/controllers/user.product.controller.js
+++ 5. NodeJS/2. Class Projects/NodeJS/myApp4/controllers/user.product.controller.js
@@ -41,3 +41,170 @@ exports.create = async (req, res) => {
 		res.json({ status: false, data: err });
 	}
 };
+
+exports.update = async (req, res) => {
+	const username = req.params.username;
+	const product_id = req.body.product._id;
+	const product_quantity = req.body.product.quantity;
+
+	console.log(`Update product quantity for user: ${username}`);
+
+	try {
+		const result = await User.updateOne(
+			{ username: username, "products._id": product_id },
+			{ $set: { "products.$.quantity": product_quantity } }
+		);
+		res.json({ status: true, data: result });
+	} catch (err) {
+		res.json({ status: false, data: err });
+	}
+};
+
+exports.delete = async (req, res) => {
+	const username = req.params.username;
+	const product_id = req.params.id;
+	const product_quantity = req.body.product.quantity;
+
+	console.log(`Delete product from user: ${username}`);
+
+	try {
+		const result = await User.deleteOne(
+			{ username: username, "products._id": product_id },
+			{ $pull: { products: { _id: product_id } } }
+		);
+		res.json({ status: true, data: result });
+	} catch (err) {
+		res.json({ status: false, data: err });
+	}
+};
+
+exports.stats1 = async (req, res) => {
+	console.log("For all users: sum by product and count");
+	try {
+		const result = await User.aggregate([
+			{
+				$unwind: "$products",
+			},
+			{
+				$group: {
+					_id: {
+						username: "$username",
+						product: "$products.product",
+					},
+					totalAmount: {
+						$sum: {
+							$multiply: ["$products.cost", "$products.quantity"],
+						},
+					},
+					count: { $sum: 1 },
+				},
+			},
+			{
+				$project: {
+					_id: 0,
+					username: "$_id.username",
+					product: "$_id.product",
+					totalAmount: 1,
+					count: 1,
+				},
+			},
+			// Sort by username in ascending order (use -1 for descending)
+			{
+				$sort: {
+					username: 1, // 1 for ascending order, -1 for descending
+				},
+			},
+		]);
+		res.json({ status: true, data: result });
+	} catch (err) {
+		res.json({ status: false, message: err.message });
+	}
+};
+
+
+exports.stats2 = async (req, res) => {
+	console.log("Stats2");
+
+	try {
+		const result = await User.aggregate([
+			{
+				$unwind: "$products",
+			},
+			{
+				$project: {
+					_id: 0,
+					products: 1,
+				},
+			},
+			{
+				$group: {
+					_id: {
+						product: "$products.product",
+					},
+					totalAmount: {
+						$sum: {
+							$multiply: ["$products.cost", "$products.quantity"],
+						},
+					},
+					count: { $sum: 1 },
+				},
+			},
+			// Sort by product in ascending order (use -1 for descending)
+			{
+				$sort: {
+					"_id.product": 1, // 1 for ascending, -1 for descending
+				},
+			},
+		]);
+		res.json({ status: true, data: result });
+	} catch (err) {
+		res.json({ status: false, message: err.message });
+	}
+};
+
+
+exports.stats3 = async (req, res) => {
+	const username = req.params.username; // Use the username from request parameters
+	console.log("Stats3 for username:", username);
+
+	try {
+		const result = await User.aggregate([
+			{
+				$match: {
+					username: username, // Match by the username
+				},
+			},
+			{
+				$unwind: "$products",
+			},
+			{
+				$project: {
+					_id: 0,
+					products: 1,
+				},
+			},
+			{
+				$group: {
+					_id: {
+						product: "$products.product",
+					},
+					totalAmount: {
+						$sum: {
+							$multiply: ["$products.cost", "$products.quantity"],
+						},
+					},
+					count: { $sum: 1 },
+				},
+			},
+			// Sort by product in ascending order (use -1 for descending)
+			{
+				$sort: {
+					"_id.product": 1, // 1 for ascending, -1 for descending
+				},
+			},
+		]);
+		res.json({ status: true, data: result });
+	} catch (err) {
+		res.json({ status: false, message: err.message });
+	}
+};
--- 5. NodeJS/2. Class Projects/NodeJS/myApp4/routes/user.product.routes.js
+++ 5. NodeJS/2. Class Projects/NodeJS/myApp4/routes/user.product.routes.js
@@ -3,9 +3,19 @@ const router = express.Router();
 
 const userProductController = require("../controllers/user.product.controller");
 
+router.get("/stats1", userProductController.stats1);
+router.get("/stats2", userProductController.stats2);
+
 router.get("/", userProductController.findAll);
-router.get("/:username", userProductController.findOne)
-router.post("/", userProductController.create)
+router.get("/:username", userProductController.findOne);
+router.post("/", userProductController.create);
+router.patch("/:username", userProductController.update);
+router.delete("/username/product/:id", userProductController.delete);
+
+router.get("/:username/stats3", userProductController.stats3);
+
+
+
 
 
 module.exports = router;
